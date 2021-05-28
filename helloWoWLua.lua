9db6949d3649df7f5dedd748f07fc8f6664e97c7
Logan <logan.scholz@gmail.com>
2021-05-27 21:00:06 -0700

    messing with frames and buttons
    messing with frames and buttons

--- helloWoWLua.lua
+++ helloWoWLua.lua
@@ -1,6 +1,6 @@
 print("HELLO WOW LUA YO OMG")
 
-message("YO DOG")
+--message("YO DOG")
 --message("MULTI MESSAGE TIME")
 --message("SUPER MESSAGE TIME")
 --message("DUPER MESSAGE TIME")
@@ -28,3 +28,53 @@ end
 printTheThings("yes", "no", "maybe", 1, "interesting", "stringly", "argument", "strings")
 
 printTheThings("hello", "wow", "toc", "addon")
+
+for i=1,5,1 do
+    CameraZoomOut(10)
+end
+
+for i=1,5,1 do
+    CameraZoomIn(10)
+end
+
+for i=1,5,1 do
+    CameraZoomOut(10)
+end
+
+for i=1,5,1 do
+    CameraZoomIn(10)
+end
+
+local function OnEvent(self, event, ...)
+	print(event, ...)
+    message(event)
+end
+
+local f = CreateFrame("Frame")
+--f:RegisterEvent("CHAT_MSG_CHANNEL")
+f:RegisterEvent("PLAYER_STARTED_MOVING")
+f:RegisterEvent("PLAYER_STOPPED_MOVING")
+f:SetScript("OnEvent", OnEvent)
+
+local f = CreateFrame("Frame", "MainFrame", nil, nil, 1)
+--local f = CreateFrame("Frame", nil)
+f:SetPoint("CENTER")
+f:SetSize(128,128)
+
+f.tex = f:CreateTexture()
+f.tex:SetAllPoints(f)
+f.tex:SetTexture("interface/icons/inv_mushroom_11")
+
+
+local function clickerino(s, b)
+    print("Clicked With: " .. b) 
+end
+
+local btn = CreateFrame("Button", nil, UIParent, "UIPanelButtonTemplate")
+btn:SetPoint("TOP")
+btn:SetSize(100,40)
+btn:SetText("Click Me")
+btn:SetScript("OnClick", 
+        function(self, button) 
+            print("Clicked With: " .. button)
+        end)
